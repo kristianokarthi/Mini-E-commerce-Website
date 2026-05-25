734579c7ab2f39784ce18d66e0c1518947b21c42
karthikeyan <80340438+kristianokarthi@users.noreply.github.com>
2026-05-25 15:04:44 +0530

    Change price display to use span tag

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -63,11 +63,11 @@ const ProductDetails = () => {
                             <div className="column rp">
                                 <h1>{title}</h1>
 
-                                <h2>
-                                    <a className="ui teal tag label">
-                                        ${price}
-                                    </a>
-                                </h2>
+                            <h2>
+  <span className="ui teal tag label">
+    ${price}
+  </span>
+</h2>
 
                                 <h3 className="ui brown block header">
                                     {category}
